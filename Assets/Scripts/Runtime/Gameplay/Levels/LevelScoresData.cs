0f12b0142ee606a761b92d604aa91d3d1872a7eb
Evrengo <136704312+Evrengo@users.noreply.github.com>
2024-07-08 20:44:56 +0300

    LevelScores Data created

---
+++ Assets/Scripts/Runtime/Gameplay/Levels/LevelScoresData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct LevelScoresData 
+{
+    public int index;
+    public string title;
+    public int highScore;
+    public bool isUnlocked;
+}
