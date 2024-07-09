011b98ce63ddf78c5873a272b86388defc5b08fb
Evrengo <136704312+Evrengo@users.noreply.github.com>
2024-07-09 20:49:15 +0300

    Level button click me animation

---
+++ Assets/Scripts/Runtime/UI/LevelsButton.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelsButton : MonoBehaviour
+{
+   void ClickMeAnimation()
+    {
+        DOTween.Sequence()
+            .Append(transform.DOPunchScale(Vector3.one * .15f, .5f).SetEase(Ease.InOutExpo))
+            .AppendInterval(.3f)
+            .SetLoops(-1, LoopType.Restart)
+            .OnKill(() =>
+            {
+                transform.localScale = Vector3.one;
+            })
+            .SetId(transform);
+    }
+}
